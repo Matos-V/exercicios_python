aa5c7c4bec21fa2cf352bc55ea5c555cdf1e071b
Wolf <vitor.matos@ee.ufcg.edu.br>
2019-04-14 20:24:48 -0300

    começando o exercicio 28

---
+++ desafio028.py
@@ -0,0 +1,11 @@
+import random
+
+nome1 = str(input('nome do primeiro aluno: '))
+nome2 = str(input('nome do segundo aluno: '))
+nome3 = str(input('nome do terceiro aluno: '))
+nome4 = str(input('nome do quarto aluno: '))
+lista = [nome1,nome2,nome3,nome4]
+random.shuffle(lista)
+n = random.randint()
+#print('O aluno sorteado para apagar o quadro foi {}'.format(n))
+print(lista)
