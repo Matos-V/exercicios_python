e0d723c8173f509054996c441d9ed18aa11950af
Matos-V <vitor.matos@ee.ufcg.edu.br>
2021-03-05 18:21:11 -0300

    ultimo commit antes de tornar o repositório público

--- scraping.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador?command=AlunoTurmasListar'
-response = requests.get(url)
-html = BeautifulSoup(response.text, 'html.parser')
-
-for tags in html.select('.container panel panel-primary text-center'):
-    print(tags)
